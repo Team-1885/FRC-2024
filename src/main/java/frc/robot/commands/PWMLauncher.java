c80ae70fe7e6bdb6b3d422daebca699f3a60ad72
Theresa <sayentm27@pwcs-edu.org>
2024-02-01 17:56:07 -0500

    added code
    added some of the kitbot code from the WPILIB1885

---
+++ src/main/java/frc/robot/commands/PWMLauncher.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+public enum PWMLauncher {
+    ;
+
+    void setLaunchWheel(Object kLauncherSpeed) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setLaunchWheel'");
+    }
+
+}
