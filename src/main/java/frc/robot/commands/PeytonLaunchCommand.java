554647c4ccf737af71ed5150b0b8ad41bcef6d54
Peyton <demichelep@icloud.com>
2024-02-10 13:19:48 -0500

    trying to fix button

--- src/main/java/frc/robot/commands/PeytonLaunchCommand.java
+++ src/main/java/frc/robot/commands/PeytonLaunchCommand.java
@@ -5,12 +5,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.common.types.input.ELogitech310;
 import frc.robot.ADAM;
-import frc.robot.RobotContainer;
-import frc.robot.commands.PeytonLaunchCommand;
-import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.PeytonLauncher;
 import lombok.Getter;
 //don't have any imports, idk what I need to import because no errors are showing
@@ -22,14 +18,11 @@ private @Getter ADAM adam = new ADAM(null);
 
 private final @Getter PeytonLauncher peytonLauncher;
 
-private final @Getter PeytonLaunchCommand peytonCANLauncher;
-
   // Creates a new PeytonCANLaunchCommand.
   public PeytonLaunchCommand(final PeytonLauncher peytonLauncher) {
     super();
     // Use addRequirements() here to declare subsystem dependencies.
     this.peytonLauncher = peytonLauncher;
-    this.peytonCANLauncher = null;
     addRequirements(peytonLauncher);
   }
 
@@ -50,7 +43,7 @@ private final @Getter PeytonLaunchCommand peytonCANLauncher;
       double launch2Speed = 0;
 
       //input from buttons
-      if(ELogitech310.A_BTN.isButton())
+      if(ELogitech310.DPAD_UP.isButton())
       {
         launchSpeed = 0.5;
         launch2Speed = 0.5;
