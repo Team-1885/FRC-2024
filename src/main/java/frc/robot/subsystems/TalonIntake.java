c6a8933a6fc9406b0ec7ef69e6a9c278f59fcfed
Peyton <demichelep@icloud.com>
2024-02-24 09:55:13 -0500

    more work

--- src/main/java/frc/robot/subsystems/TalonIntake.java
+++ src/main/java/frc/robot/subsystems/TalonIntake.java
@@ -6,9 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class TalonsIntake extends SubsystemBase {
-  /** Creates a new TalonsIntake. */
-  public TalonsIntake() {}
+public class TalonIntake extends SubsystemBase {
+  /** Creates a new TalonIntake. */
+  public TalonIntake() {}
 
   @Override
   public void periodic() {
