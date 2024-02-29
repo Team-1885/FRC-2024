96db451b7cae50012526b13f48ebe77514069884
BigManSam1885 <LeeK26@pwcs-edu.org>
2024-02-29 15:45:52 -0500

    michael recalc
    recalc

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -25,12 +25,12 @@ public final class RobotMap {
 
     // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
     // These characterization values MUST be determined either experimentally or theoretically for *your* robot's drive. The Robot Characterization Toolsuite provides a convenient tool for obtaining these values for your robot.
-    public static final double ksVolts = 0.22;
-    public static final double kvVoltSecondsPerMeter = 1.98;
-    public static final double kaVoltSecondsSquaredPerMeter = 0.2;
+    public static final double ksVolts = 0.25;
+    public static final double kvVoltSecondsPerMeter = 2.16;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.48;
 
     // Example value only - as above, this must be tuned for your drive!
-    public static final double kPDriveVel = 8.5;
+    public static final double kPDriveVel = 5;
 
     public static final double kGearRatio = 8.46;
     public static final double kWheelRadiusInches = 3;
